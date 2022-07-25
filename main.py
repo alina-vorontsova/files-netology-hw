d299e312efda9c9b44353c58add179134c32a513
Alina Vorontsova <vorontsova-alina@mail.ru>
2022-07-25 22:12:40 +0300

    main.py update

--- main.py
+++ main.py
@@ -17,31 +17,32 @@ with open('recipes.txt', encoding='utf-8') as file_object:
         cook_book[dish_name] = ingredients_list
         file_object.readline()
 
-#pprint(cook_book, sort_dicts=False, width=100)
-
 def get_shop_list_by_dishes(dishes, person_count):
     shop_dict = {}
+    #ingredients_info = {}
     for item in dishes: 
         if item not in cook_book.keys():
             error = 'Ошибка: введённого Вами блюда нет в кулинарной книге'
-            return error 
+            return error
         for key, values in cook_book.items():
             if item in key:
                 ingredients_info = {}
                 for value in values:
                     ingredients_info = {}
-                    shop_dict_ingredient = value['ingredient_name'] 
-                    if shop_dict_ingredient not in shop_dict.keys():
-                        shop_dict[shop_dict_ingredient] = {}
-                        ingredients_info['measure'] = value['measure']
+                    shop_dict_ingredient = value['ingredient_name']
+                    shop_dict[shop_dict_ingredient] = ingredients_info
+                    ingredients_info['measure'] = value['measure']
+                    if 'quantity' not in ingredients_info.keys():
                         ingredients_info['quantity'] = int(value['quantity']) * person_count
-                        shop_dict[shop_dict_ingredient] = ingredients_info
-                    else: 
+                    else:
                         new_quantity = ingredients_info['quantity'] + (int(value['quantity']) * person_count)
                         ingredients_info['quantity'] = new_quantity
-    pprint(shop_dict, sort_dicts=False)
+    return shop_dict
 
+#pprint(cook_book, sort_dicts=False, width=100)
+#pprint(get_shop_list_by_dishes(['Омлет', 'Утка по-пекински', 'Запеченный картофель'], 3), sort_dicts=False)
 
-
-print(get_shop_list_by_dishes(['Омлет', 'Запеченный картофель'], 2))
-
+# упорно не получается обыграть сценарий с повторением игредиентов, гуглила и спрашивала в дискорде и телеграме 
+# в соответствующих чатах - тоже не смогли помочь. Если оставляю в третьем цикле, то не прибавляется значение к повторяющемуся
+# ингредиенту, если ставлю в циклы раньше, то количества неверные уже у всех игнредиентов. 
+pprint(get_shop_list_by_dishes(['Омлет', 'Фахитос'], 2), sort_dicts=False) 
